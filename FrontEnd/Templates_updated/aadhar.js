51f6a0c8f1c48a52552f1544a997a80b651e258a
Kishore Prashanth <kishorep.shrivatsa@gmail.com>
2021-06-24 09:35:57 +0530

    Add files via upload

---
+++ FrontEnd/Templates_updated/aadhar.js
@@ -0,0 +1,43 @@
+function readURL(input) {
+  if (input.files && input.files[0]) {
+
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      $('.image-upload-wrap').hide();
+
+      $('.file-upload-image').attr('src', e.target.result);
+      $('.file-upload-content').show();
+
+      $('.image-title').html(input.files[0].name);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+
+    // Bind `input` event on both the inputs
+$('.file-upload-input').on('input', function() {
+  // toggle: If argument passed is 
+  //         true:  show
+  //         false: hide
+  window.alert('Ganesh );
+}).trigger('input'); // Trigger event to call on page load
+
+  } 
+  
+  else {
+    removeUpload();
+  }
+}
+
+function removeUpload() {
+  $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+  $('.file-upload-content').hide();
+  $('.image-upload-wrap').show();
+}
+
+$('.image-upload-wrap').bind('dragover', function () {
+  $('.image-upload-wrap').addClass('image-dropping');
+});
+$('.image-upload-wrap').bind('dragleave', function () {
+  $('.image-upload-wrap').removeClass('image-dropping');
+});
